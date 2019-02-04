3454a4959a0380ccb5f617e298ce4e78ce134177
lukaszreplin <lukaszreplin@gmail.com>
2019-02-04 20:00:38 +0100

    Added swagger

--- PictureBase/Startup.cs
+++ PictureBase/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PictureBase.BusinessLogic.Contracts;
 using PictureBase.BusinessLogic.Services;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace PictureBase
 {
@@ -22,6 +23,11 @@ namespace PictureBase
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddScoped<IImagesManager, ImagesManager>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "PictureBase API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +41,10 @@ namespace PictureBase
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "PictureBase API V1"); });
 
             app.UseHttpsRedirection();
             app.UseMvc();
